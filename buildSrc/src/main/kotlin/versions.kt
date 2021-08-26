4323c579c0cf291ee7f7b3b3bca1c9ac5c99a0a9
andylamax <andylamax@programmer.net>
2021-08-26 04:50:26 +0300

    cleaned the credentials in workfile

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -17,8 +17,8 @@ object vers {
     }
 
     object bitframe {
-        val current = "0.0.16"
-        val previous = "0.0.15"
+        val current = "0.0.17"
+        val previous = "0.0.16"
     }
 
     object asoft {
