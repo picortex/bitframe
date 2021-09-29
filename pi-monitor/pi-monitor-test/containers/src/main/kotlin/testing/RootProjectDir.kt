168be44f71ba82e9d20ebdf042dcabcf7880d749
andylamax <andylamax@programmer.net>
2021-09-29 11:36:12 +0300

    Corrected RootProjectDir for container to properly resolve directory

--- pi-monitor/pi-monitor-test/containers/src/main/kotlin/testing/RootProjectDir.kt
+++ pi-monitor/pi-monitor-test/containers/src/main/kotlin/testing/RootProjectDir.kt
@@ -1,13 +1,12 @@
 package testing
 
-import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.absolutePathString
 
 class RootProjectDir {
     companion object {
         fun getPath(): String {
-            val path = Path(".").absolutePathString()
+            val path = Path(".").absolutePathString().replace("/bitframe/bitframe/", "/bitframe/")
             val parentDir = path.split("/bitframe/")
             return Path(parentDir.first() + "/bitframe").absolutePathString()
         }
