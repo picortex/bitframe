16f5660259bef76e92409d08116d3ca3c65162be
andylamax <andylamax@programmer.net>
2021-11-24 20:56:04 +0300

    Preparing to build

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,15 +1,65 @@
-object vers {
+//object vers {
 //    val kotlin = "1.6.0" // this
 //
 //    val ksp = "1.6.0-1.0.1" // this
+//
+//    val kotlinpoet = "1.12.1"
+//
+//    val ktor = "1.6.5"
+//
+//    val selenide = "5.23.0"
+//
+//    val docker = "7.1.0"
+//
+//    val dokka = "1.5.0"
+//
+//    val npmPublish = "2.0.3"
+//
+//    val testContainers = "1.16.0"
+//
+//    object kotlinx {
+//        val coroutines = "1.5.2-native-mt"
+//        val serialization = "1.3.0"
+//        val datetime = "0.3.1"
+//    }
+//
+//    object bitframe {
+//        val current = "0.0.40"
+//        val previous = "0.0.38"
+//    }
+//
+//    object npm {
+//        val asyncStorage = "1.15.9"
+//    }
+//
+//    object asoft {
+//        val foundation = "1.4.30" // this
+//        val platform = foundation
+//        val contacts = "0.0.30"
+//        val duality = "0.0.50"
+//        val collections = foundation
+//        val mvivm = "0.1.21"
+//        val viewmodel = mvivm
+//        val live = mvivm
+//        val builders = foundation
+//        val reakt = "0.1.20"
+//        val later = foundation
+//        val logging = foundation
+//        val krypto = "0.0.50"
+//        val kash = "0.0.10"
+//        val mapper = foundation
+//        val expect = foundation
+//    }
+//}
 
+object vers {
     val kotlin = "1.5.31" // this
 
     val ksp = "1.5.31-1.0.0" // this
 
     val kotlinpoet = "1.12.1"
 
-    val ktor = "1.6.5"
+    val ktor = "1.6.2"
 
     val selenide = "5.23.0"
 
@@ -23,26 +73,22 @@ object vers {
 
     object kotlinx {
         val coroutines = "1.5.2-native-mt"
-        val serialization = "1.3.0"
-        val datetime = "0.3.1"
+        val serialization = "1.2.2"
+        val datetime = "0.2.1"
     }
 
     object bitframe {
-        val current = "0.0.40"
-        val previous = "0.0.38"
-    }
-
-    object npm {
-        val asyncStorage = "1.15.9"
+        val current = "0.0.36"
+        val previous = "0.0.35"
     }
 
     object asoft {
-        val foundation = "1.4.30" // this
+        val foundation = "1.4.11" // this
         val platform = foundation
         val contacts = "0.0.30"
         val duality = "0.0.50"
         val collections = foundation
-        val mvivm = "0.1.21"
+        val mvivm = "0.1.20"
         val viewmodel = mvivm
         val live = mvivm
         val builders = foundation
