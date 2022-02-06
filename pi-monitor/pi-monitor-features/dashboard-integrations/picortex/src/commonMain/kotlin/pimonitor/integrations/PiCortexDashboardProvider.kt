8bf1d23ba20b37dc7f8864efbc9d599a26f966f7
andylamax <andylamax@programmer.net>
2022-02-06 14:22:51 +0300

    Fixed failing CI

--- pi-monitor/pi-monitor-features/dashboard-integrations/picortex/src/commonMain/kotlin/pimonitor/integrations/PiCortexDashboardProvider.kt
+++ pi-monitor/pi-monitor-features/dashboard-integrations/picortex/src/commonMain/kotlin/pimonitor/integrations/PiCortexDashboardProvider.kt
@@ -1,10 +1,10 @@
 package pimonitor.integrations
 
-import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.http.content.*
+import io.ktor.util.*
 import kotlinx.serialization.mapper.Mapper
 import later.later
 
@@ -16,21 +16,19 @@ class PiCortexDashboardProvider(
     private val client get() = config.client
     private val parser get() = config.parser
 
+    @OptIn(InternalAPI::class)
     fun technicalDashboardOf(credentials: PiCortexUserCredentials) = scope.later<OperationalDashboard> {
-        val res = try {
-            val map = mapOf(
-                "secret" to credentials.secret,
-                "userType" to "DataConsoleUser"
+
+        val map = mapOf(
+            "secret" to credentials.secret,
+            "userType" to "DataConsoleUser"
+        )
+        val res = client.post("https://${credentials.subdomain}.picortex.com/api/reporting") {
+            body = TextContent(
+                text = Mapper.encodeToString(map),
+                contentType = ContentType.Application.Json
             )
-            client.post("https://${credentials.subdomain}.picortex.com/api/reporting") {
-                body = TextContent(
-                    text = Mapper.encodeToString(map),
-                    contentType = ContentType.Application.Json
-                )
-            }
-        } catch (exp: ResponseException) {
-            exp.response
         }
-        parser.parseTechnicalDashboard(res.readText())
+        parser.parseTechnicalDashboard(res.bodyAsText())
     }
 }
