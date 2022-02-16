e131b1045a118ad18b2c3761f0d0c1e01f5d6f47
andylamax <andylamax@programmer.net>
2022-02-16 03:02:26 +0300

    Exported the necessary methods

--- pimonitor/pimonitor-api/public/src/jsMain/kotlin/ApiEntrypoint.kt
+++ pimonitor/pimonitor-api/public/src/jsMain/kotlin/ApiEntrypoint.kt
@@ -4,6 +4,7 @@ import bitframe.client.configurators.toValidApiConfigurator
 import pimonitor.client.PiMonitorApi
 import pimonitor.client.toPiMonitorApi
 
+@JsExport
 fun api(configurator: ApiConfigurator): PiMonitorApi {
     val cfg = configurator.toValidApiConfigurator()
     return cfg.toApiMode().toPiMonitorApi()
--- pimonitor/pimonitor-sdk/client/react/src/main/kotlin/SdkEntrypoint.kt
+++ pimonitor/pimonitor-sdk/client/react/src/main/kotlin/SdkEntrypoint.kt
@@ -1,8 +1,8 @@
-package pimonitor.client
-
 import bitframe.client.configurators.SdkConfigurator
+import pimonitor.client.PiMonitorReactAppScope
 import pimonitor.client.configurtors.toPiMonitorAppScopeConfig
 
+@JsExport
 fun scope(configurator: SdkConfigurator): PiMonitorReactAppScope {
     val config = configurator.toPiMonitorAppScopeConfig()
     return PiMonitorReactAppScope(config)
