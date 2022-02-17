11b6c9afe5320444c76db78d92644579a477ed76
andylamax <andylamax@programmer.net>
2022-02-17 05:42:13 +0300

    Updated version for scripting

--- .github/workflows/commit.main.kts
+++ .github/workflows/commit.main.kts
@@ -1,6 +1,6 @@
 #!/usr/bin/env kotlin
 
-@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.6.0")
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.8.0")
 
 import it.krzeminski.githubactions.actions.Action
 import it.krzeminski.githubactions.actions.actions.CheckoutV2
