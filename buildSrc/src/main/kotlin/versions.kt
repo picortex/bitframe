b3755599fe6992e997ff2b4fbfb979c52adbc7d7
andylamax <andylamax@programmer.net>
2022-02-21 07:00:27 +0300

    Removed unused versions

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -5,23 +5,14 @@ object vers {
 
     val kotlinpoet = "1.12.1"
 
-    val selenide = "5.23.0"
-
     val docker = "7.1.0"
 
     val dokka = "1.5.0"
 
     val npmPublish = "2.1.2"
 
-    val testContainers = "1.16.0"
-
-
     object bitframe {
         val current = "0.0.65"
         val previous = "0.0.52"
-    }
-
-    object npm {
-        val asyncStorage = "1.15.9"
     }
 }
