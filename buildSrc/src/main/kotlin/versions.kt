48ec95257096e486cbec509d31923937e81835f0
andylamax <andylamax@programmer.net>
2022-02-21 18:05:35 +0300

    Removed .npmrc in the files

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -12,7 +12,7 @@ object vers {
     val npmPublish = "2.0.3"// "2.1.2"
 
     object bitframe {
-        val current = "0.0.66"
-        val previous = "0.0.65"
+        val current = "0.0.67"
+        val previous = "0.0.66"
     }
 }
