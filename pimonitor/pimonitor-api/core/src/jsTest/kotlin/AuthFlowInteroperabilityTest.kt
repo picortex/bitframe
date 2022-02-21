64376327a21c9150eb19c175671f77d379ec494e
andylamax <andylamax@programmer.net>
2022-02-21 15:08:27 +0300

    Fixed failing CI

--- pimonitor/pimonitor-api/core/src/jsTest/kotlin/AuthFlowInteroperabilityTest.kt
+++ pimonitor/pimonitor-api/core/src/jsTest/kotlin/AuthFlowInteroperabilityTest.kt
@@ -2,6 +2,7 @@ import bitframe.client.jso
 import expect.expect
 import expect.expectFailure
 import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Clock
 import later.await
 import pimonitor.client.PiMonitorApi
 import pimonitor.client.PiMonitorApiMock
@@ -15,13 +16,14 @@ class AuthFlowInteroperabilityTest {
 
     @Test
     fun should_register_a_monitor_as_a_valid_user() = runTest {
+        val time = Clock.System.now()
         val params = jso<dynamic> {
-            name = "John Doe 1"
-            email = "john@doe1.com"
+            name = "John Doe $time"
+            email = "john@doe$time.com"
             password = "john@doe1.com"
         }.unsafeCast<IndividualSignUpRawParams>()
         val res = service.signUp.signUp(params).await()
-        expect(res.user.name).toBe("John Doe 1")
+        expect(res.user.name).toBe("John Doe $time")
     }
 
     @Test
