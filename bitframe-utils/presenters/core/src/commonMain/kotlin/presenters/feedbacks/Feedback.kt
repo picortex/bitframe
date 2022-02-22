eadba8256b799cb4646e3d06011f1d8632b09e40
andylamax <andylamax@programmer.net>
2022-02-22 04:21:56 +0300

    Fixed failing Ci tests https://github.com/picortex/bitframe/runs/5281816225?check_suite_focus=true

--- bitframe-utils/presenters/core/src/commonMain/kotlin/presenters/feedbacks/Feedback.kt
+++ bitframe-utils/presenters/core/src/commonMain/kotlin/presenters/feedbacks/Feedback.kt
@@ -8,22 +8,44 @@ import kotlin.js.JsExport
 sealed class Feedback {
     abstract val message: String
 
-    data class Loading(
+    class Loading(
         override val message: String
     ) : Feedback() {
         val loading: Boolean = true
+        override fun hashCode(): Int = message.hashCode()
+        override fun equals(other: Any?): Boolean = when (other) {
+            is Loading -> other.message == message
+            else -> false
+        }
+
+        override fun toString(): String = "Loading(message=$message)"
     }
 
-    data class Failure(
+    class Failure(
         val cause: Throwable? = null,
         override val message: String = cause?.message ?: "Unknown error"
     ) : Feedback() {
         val failure: Boolean = true
+        override fun hashCode(): Int = message.hashCode()
+        override fun equals(other: Any?): Boolean = when (other) {
+            is Failure -> other.message == message
+            else -> false
+        }
+
+        override fun toString(): String = "Failure(message=$message)"
     }
 
-    data class Success(
+    class Success(
         override val message: String = "Success"
     ) : Feedback() {
         val success = true
+        override fun hashCode(): Int = message.hashCode()
+
+        override fun equals(other: Any?): Boolean = when (other) {
+            is Success -> other.message == message
+            else -> false
+        }
+
+        override fun toString(): String = "Success(message=$message)"
     }
 }
