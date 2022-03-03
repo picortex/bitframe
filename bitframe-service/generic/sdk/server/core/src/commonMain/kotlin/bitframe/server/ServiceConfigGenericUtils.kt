63e10623004846ecd499ec63173bb75e37cdffb9
andylamax <andylamax@programmer.net>
2022-03-03 05:23:24 +0300

    Tests are passing now

--- bitframe-service/generic/sdk/server/core/src/commonMain/kotlin/bitframe/server/ServiceConfigGenericUtils.kt
+++ bitframe-service/generic/sdk/server/core/src/commonMain/kotlin/bitframe/server/ServiceConfigGenericUtils.kt
@@ -1,5 +1,5 @@
 package bitframe.server
 
 inline fun <reified T : Any> ServiceConfig.toGenericServiceConfig() = GenericServiceConfig(
-    T::class, daoFactory, bus, logger, json, scope
+    T::class, daoFactory, bus, logger, mailer, json, scope
 )
