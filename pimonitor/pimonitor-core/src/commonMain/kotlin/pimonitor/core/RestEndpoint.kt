bc81f25ac262c5281851c492a2660af4f246128c
andylamax <andylamax@programmer.net>
2022-03-15 17:02:44 +0300

    Fixed pimonitor mock api tests

--- pimonitor/pimonitor-core/src/commonMain/kotlin/pimonitor/core/RestEndpoint.kt
+++ pimonitor/pimonitor-core/src/commonMain/kotlin/pimonitor/core/RestEndpoint.kt
@@ -4,6 +4,13 @@ sealed class RestEndpoint(private val root: String) {
     class Client(val url: String) : RestEndpoint("$url/api")
     class Server() : RestEndpoint("/api")
 
+    //businesses
+    private val businesses = "$root/businesses"
+    val businessesAll = "$businesses/all"
+    val businessesCreate = "$businesses/create"
+    val businessesDelete = "$businesses/delete"
+    val businessesDashboard = "$businesses/dashboard"
+
     // invites
     val invitesAcceptSage = "$root/invites/accept/sage"
     val invitesAcceptPicortex = "$root/invites/accept/picortex"
