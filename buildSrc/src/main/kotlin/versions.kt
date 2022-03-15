5321bc20f61526808d31ff9e87005644e73cddb9
andylamax <andylamax@programmer.net>
2022-03-16 02:58:30 +0300

    Bumped staging version from 0.0.89 to 0.0.90

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -12,9 +12,9 @@ object vers {
     val npmPublish = "2.1.2"
 
     object bitframe {
-        private val latest = "0.0.89"
+        private val latest = "0.0.90"
         val stagingCurrent = latest
-        val stagingPrevious = "0.0.88"
+        val stagingPrevious = "0.0.89"
         val productionCurrent = latest
         val productionPrevious = "0.0.80"
     }
