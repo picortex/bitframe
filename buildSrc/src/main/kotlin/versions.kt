92829000a07085f592964787c13f12583084fe3f
andylamax <andylamax@programmer.net>
2022-04-01 05:56:37 +0300

    Bumped up version

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -6,9 +6,9 @@ object vers {
     val npmPublish = "2.1.2"
 
     object bitframe {
-        private val latest = "0.0.98"
+        private val latest = "0.0.99"
         val stagingCurrent = latest
-        val stagingPrevious = "0.0.97"
+        val stagingPrevious = "0.0.98"
         val productionCurrent = latest
         val productionPrevious = "0.0.90"
     }
