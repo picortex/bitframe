8eebfecce39cda367da61cfc4a170aaeed2a41cf
andylamax <andylamax@programmer.net>
2022-04-04 23:24:55 +0300

    Fixed failing tests

--- bitframe-features/authentication/api/ktor/src/commonMain/kotlin/bitframe/client/signin/SignInServiceKtor.kt
+++ bitframe-features/authentication/api/ktor/src/commonMain/kotlin/bitframe/client/signin/SignInServiceKtor.kt
@@ -4,6 +4,7 @@ import bitframe.client.ServiceConfigKtor
 import bitframe.client.of
 import bitframe.client.rest.pathV1
 import bitframe.core.RequestBody
+import bitframe.core.signin.SignInParams
 import bitframe.core.signin.SignInRawParams
 import bitframe.core.signin.SignInResult
 import io.ktor.client.request.*
@@ -18,7 +19,7 @@ open class SignInServiceKtor(
     private val http get() = config.http
     private val json get() = config.json
 
-    override fun signIn(rb: RequestBody.UnAuthorized<SignInRawParams>): Later<SignInResult> = scope.later {
+    override fun signIn(rb: RequestBody.UnAuthorized<SignInParams>): Later<SignInResult> = scope.later {
         val resp = http.post(config.pathV1.signIn) { setBody(json.of(rb)) }
         json.decodeResponseFromString(SignInResult.serializer(), resp.bodyAsText()).response()
     }
--- bitframe-service/builder/sdk/client/core/src/commonMain/kotlin/bitframe/client/MicroScopeBuilders.kt
+++ bitframe-service/builder/sdk/client/core/src/commonMain/kotlin/bitframe/client/MicroScopeBuilders.kt
@@ -9,19 +9,16 @@ fun <I, S> MicroScope(
     @BuilderInference builder: MicroScopeBuilder<I, S>.() -> Unit
 ): Lazy<MicroScope<I, S>> = lazy {
     val scopeBuilder = MicroScopeBuilder<I, S>().apply(builder)
-    object : MicroScope<I, S> {
-        override val viewModel = scopeBuilder.viewModel
-        override val intents = scopeBuilder.intents
-    }
+    MicroScopeImpl(scopeBuilder.viewModel, scopeBuilder.intents)
 }
 
 fun <I, S, C> FullMicroScope(
     @BuilderInference builder: MicroScopeWithConstantsBuilder<I, S, C>.() -> Unit
 ): Lazy<MicroScopeWithConstants<I, S, C>> = lazy {
     val scopeBuilder = MicroScopeWithConstantsBuilder<I, S, C>().apply(builder)
-    object : MicroScopeWithConstants<I, S, C> {
-        override val viewModel = scopeBuilder.viewModel
-        override val intents = scopeBuilder.intents
-        override val constants = scopeBuilder.constants
-    }
+    MicroScopeWithConstantsImpl(
+        viewModel = scopeBuilder.viewModel,
+        intents = scopeBuilder.intents,
+        constants = scopeBuilder.constants,
+    )
 }
