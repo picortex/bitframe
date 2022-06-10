32b95736c2aa120ef80a1e6059cbd68bb27f029b
andylamax <andylamax@programmer.net>
2022-06-10 21:06:58 +0300

    Lifted limitations of the bitframe server

--- bitframe-sdk/server/ktor/src/main/kotlin/bitframe/server/ApplicationBuilders.kt
+++ bitframe-sdk/server/ktor/src/main/kotlin/bitframe/server/ApplicationBuilders.kt
@@ -3,13 +3,14 @@ package bitframe.server
 import kotlin.experimental.ExperimentalTypeInference
 
 @OptIn(ExperimentalTypeInference::class)
-inline fun <S : BitframeService> bitframeApplication(
+inline fun <S> bitframeApplication(
     @BuilderInference builder: KtorServerConfigurationBuilder<S>.() -> Unit
 ): Application<S> {
     val configBuilder = KtorServerConfigurationBuilder<S>().apply(builder)
     val applicationConfig = configBuilder.buildApplicationConfig().apply {
+        val service = configBuilder.buildService()
         configBuilder.moduleBuilders.forEach {
-            modules.add(it(configBuilder.buildService()))
+            modules.add(it(service))
         }
     }
     return Application(applicationConfig)
--- bitframe-sdk/server/test/src/commonMain/kotlin/bitframe/ComponentUnderTest.kt
+++ bitframe-sdk/server/test/src/commonMain/kotlin/bitframe/ComponentUnderTest.kt
@@ -7,7 +7,7 @@ import bitframe.server.http.HttpRoute
 import bitframe.server.Module
 
 sealed class ComponentUnderTest
-class ApplicationUnderTest<S : BitframeService, A : BitframeApplication<S>>(val application: A) : ComponentUnderTest()
+class ApplicationUnderTest<S, A : BitframeApplication<S>>(val application: A) : ComponentUnderTest()
 class ModuleUnderTest<M : Module>(val module: M) : ComponentUnderTest()
 class RouteUnderTest(val route: HttpRoute) : ComponentUnderTest()
 class ActionUnderTest(val action: Action) : ComponentUnderTest()
