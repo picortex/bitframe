2c42ee5e2561b0767b4583d36c0d8833dc007379
andylamax <andylamax@programmer.net>
2022-11-04 02:21:42 +0300

    Releasing picortex version 0.10.04

--- api/core/src/commonMain/kotlin/bitframe/client/BestCache.kt
+++
@@ -1,7 +0,0 @@
-package bitframe.client
-
-import cache.Cache
-import koncurrent.Executor
-import kotlinx.serialization.StringFormat
-
-expect fun BestCache(namespace: String, codec: StringFormat, executor: Executor): Cache
