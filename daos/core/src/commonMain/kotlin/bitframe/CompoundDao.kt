c0c53c36983704be3eca4c9187a36aab2800add4
andylamax <andylamax@programmer.net>
2022-12-29 14:00:33 +0300

    fixed maximum call stack for concurrent

--- daos/core/src/commonMain/kotlin/bitframe/CompoundDao.kt
+++ daos/core/src/commonMain/kotlin/bitframe/CompoundDao.kt
@@ -7,37 +7,39 @@ import bitframe.dao.exceptions.EntityNotFoundException
 import bitframe.dao.exceptions.MissingDaoException
 import bitframe.dao.internal.AbstractDao
 import koncurrent.Later
+import koncurrent.FailedLater
+import koncurrent.Laters
 import koncurrent.later.filterSuccessValues
 import kotlinx.collections.interoperable.toInteroperableList
 
-class CompoundDao<out T : Savable>(val config: CompoundDaoConfig<T>) : AbstractDao<T>() {
+class CompoundDao<T : Savable>(val config: CompoundDaoConfig<T>) : AbstractDao<T>() {
 
     private val daos by lazy { config.daos }
 
-    override fun create(input: @UnsafeVariance T): Later<out T> {
-        return daos[input::class]?.create(input) ?: Later.reject(MissingDaoException(input::class, this))
+    override fun create(input: @UnsafeVariance T): Later<T> {
+        return daos[input::class]?.create(input) ?: FailedLater(MissingDaoException(input::class, this))
     }
 
-    override fun update(obj: @UnsafeVariance T): Later<out T> {
-        return daos[obj::class]?.update(obj) ?: Later.reject(MissingDaoException(obj::class, this))
+    override fun update(obj: @UnsafeVariance T): Later<T> {
+        return daos[obj::class]?.update(obj) ?: FailedLater(MissingDaoException(obj::class, this))
     }
 
     private inline fun <R> inAllDaos(
         actions: Dao<T>.() -> Later<R>
-    ) = Later.all(*daos.values.map { it.actions() }.toTypedArray())
+    ) = Laters(*daos.values.map { it.actions() }.toTypedArray())
 
-    override fun load(uid: String): Later<out T> = inAllDaos {
+    override fun load(uid: String): Later<T> = inAllDaos {
         load(uid)
     }.then {
         it.filterSuccessValues().first()
     }
 
-    override fun delete(uid: String): Later<out T> = inAllDaos {
+    override fun delete(uid: String): Later<T> = inAllDaos {
         delete(uid)
     }.then {
         it.filterSuccessValues().firstOrNull() ?: throw EntityNotFoundException(uid)
     }.andThen {
-        daos[it::class]?.delete(uid) ?: Later.reject(MissingDaoException(it::class, this))
+        daos[it::class]?.delete(uid) ?: FailedLater(MissingDaoException(it::class, this))
     }
 
     override fun execute(query: Query) = inAllDaos {
