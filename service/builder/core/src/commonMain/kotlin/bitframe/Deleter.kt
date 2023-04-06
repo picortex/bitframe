46a4cbc041a54a726280a59583769e68e6b65694
andylamax <andylamax@programmer.net>
2023-04-06 08:25:13 +0300

    Preparing to release picortex version 0.02.27

--- service/builder/core/src/commonMain/kotlin/bitframe/Deleter.kt
+++ service/builder/core/src/commonMain/kotlin/bitframe/Deleter.kt
@@ -7,7 +7,7 @@ import kollections.List
 import koncurrent.Later
 import kotlin.js.JsExport
 
-interface Deleter<out R> {
-    fun delete(uid: String): Later<R>
-    fun deleteBulk(ids: List<String>): Later<List<R>>
+interface Deleter<out R : Any> {
+    fun delete(uid: String): Later<R?>
+    fun deleteBulk(ids: List<String>): Later<List<R?>>
 }
